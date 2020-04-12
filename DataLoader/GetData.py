cf0d6bc0cbefc93509e146495af64806484bc771
Pandian <5630870+pandian-raja@users.noreply.github.com>
2020-04-11 20:32:03 -0700

    Update GetData.py

--- DataLoader/GetData.py
+++ DataLoader/GetData.py
@@ -7,16 +7,17 @@ import numpy as np
 import albumentations as A
 from torch.utils.data import DataLoader, Dataset
 from albumentations.pytorch import ToTensor
-from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, Cutout, VerticalFlip, Rotate, RGBShift
+from albumentations import Compose, RandomCrop,PadIfNeeded,Flip, Normalize, HorizontalFlip, Cutout, VerticalFlip, Rotate, RGBShift
 from albumentations.pytorch import ToTensor
+
 class albumCompose:
     def __init__(self):
         self.albumentations_transform = Compose({
-            Cutout(max_h_size=4,max_w_size=4,num_holes=2,p=0.5),
-            HorizontalFlip(p=0.5),
-#             VerticalFlip(p=0.5),
-            Rotate((-30.0, 30.0)),
-            RGBShift(30,30,30),
+            PadIfNeeded(min_height=40,min_width=40),
+            RandomCrop(32,32),
+            Flip(),
+            VerticalFlip(p=0.5)
+            Cutout(max_h_size=8,max_w_size=8,num_holes=1),
             Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784))
         })
     def __call__(self, img):
@@ -54,6 +55,7 @@ class GetData():
         # train_transforms = transforms.Compose([
         #                                 transforms.RandomCrop(32, padding=4),
         #                                 transforms.RandomHorizontalFlip(),
+        #                                 transforms.Cutout()
         #                                 transforms.ToTensor(),
         #                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # The mean and std have to be sequences (e.g., tuples), therefore you should add a comma after the values.
         #                                ])
@@ -63,12 +65,12 @@ class GetData():
         #                                ])
         trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=albumCompose())
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=512,
+                                              shuffle=True, num_workers=4)
         testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=albumCompose_test())
-        testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=512,
+                                             shuffle=False, num_workers=4)
         classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         return trainloader, testloader, classes, device
